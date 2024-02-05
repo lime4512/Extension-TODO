e146102f7754351752d3a47faa39ea0c0f560c65
Alex Kruglov <lime4512@mail.ru>
2024-02-05 15:41:05 +0300

    add saved local storage

--- script/script.js
+++ script/script.js
@@ -2,33 +2,32 @@ const input = document.getElementById('input')
 const btn = document.getElementById('btn')
 const list = document.getElementById('list')
 
-const mainlist = []
-
 function render() {
 	list.innerHTML = ''
-	if (mainlist.length == 0) {
-		list.innerHTML = '<li>Заметок нет</li>'
-	}
-	for (let i = 0; i < mainlist.length; i++) {
+	const note = JSON.parse(localStorage.getItem('note')) || []
+
+	note.map(item => {
 		list.insertAdjacentHTML(
 			'beforeend',
 			`<li class='item-list'>
       <input type="checkbox" class="checkbox-list"/>
-      <label class="text-list">${mainlist[i].text}</label>
+      <label class="text-list">${item.text}</label>
     </li>`
 		)
-	}
+	})
 }
-
 render()
-
 btn.onclick = () => {
 	if (input.value != '') {
 		const newNote = {
 			text: input.value,
 		}
+		const mainlist = JSON.parse(localStorage.getItem('note')) || []
 		mainlist.push(newNote)
-		render()
+
+		localStorage.setItem('note', JSON.stringify(mainlist))
+		console.log(mainlist)
 	}
 	input.value = ''
+	render()
 }
